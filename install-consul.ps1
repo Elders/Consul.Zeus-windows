dd0ce4fc66b60550e0e2f04767721c3ef510b195
aroticoz <aroticoz@gmail.com>
2018-06-04 11:27:08 +0300

    Updated Consul to 1.1.0.
    Changed installation script to PowerShell. Includes the options to provide all Consul and Zeus Parameters from the command line.
    Default install switched to agent.

---
+++ install-consul.ps1
@@ -0,0 +1,75 @@
+param(
+    [string]$AzureSubscriptionId,
+
+    [string]$AzureTenantId,
+
+    [string]$AzureClientId,
+
+    [string]$AzureSecret,
+
+    [string]$AzureTagName = 'consul',
+
+    [string]$AzureTagValue, 
+
+    [string]$localnodeIP, 
+
+    [Parameter(mandatory = $true)]
+    [string[]]$ConsulArgs,
+
+    [string[]]$ZeusArgs = ("machine","-f"),
+
+    [string[]]$CheckInterval = '1m'
+)
+
+Add-Type -AssemblyName System.Web
+
+if (!$localnodeIP) {
+    $temp = Get-NetIpaddress | Where addressstate -EQ preferred | Where PrefixOrigin -EQ Dhcp | Select IPAddress
+    $localnodeIP = $temp.IPAddress
+}
+
+$AzureSecret   = [System.Web.HttpUtility]::UrlEncode($AzureSecret)
+$path = (Get-Item -Path ".\").FullName
+$ZeusArgs = ,"zeus\Zeus.exe" + $ZeusArgs
+$jsonConfig = [ordered]@{
+    data_dir= "data"
+    check_update_interval="$CheckInterval"
+    ports =@{
+        http=8500
+    }
+    log_level= "err"
+    checks = @(
+            @{
+                id="sys-health"
+			    name="System Information"
+			    args=$ZeusArgs
+			    interval="$CheckInterval"
+			    timeout="1m"
+            }
+        )
+    enable_script_checks=[bool]"true"
+}
+
+$jsonConfig | ConvertTo-Json -Depth 4 | Out-File .\config\config.json -NoNewLine -Encoding default
+
+$destination = "$path\consul.zip"
+[Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12
+$client = New-Object System.Net.WebClient
+$client.DownloadFile("https://releases.hashicorp.com/consul/1.1.0/consul_1.1.0_windows_amd64.zip", $destination)
+
+Expand-Archive -path "$path\consul.zip"  -DestinationPath $path -Force
+
+if (!$AzureSubscriptionId -and !$AzureClientId -and !$AzureTenantId -and !$AzureSecret -and !$AzureTagValue) {
+    Write-Host "Configuring without Azure integration"
+} 
+
+elseif (!$AzureSubscriptionId -or !$AzureClientId -or !$AzureTenantId -or !$AzureSecret -or !$AzureTagValue) {
+    return Write-Host "All Azure parameters are required to configure Azure integration."
+}
+
+else {
+    "{ `"retry_join`": [`"provider=azure tag_name=$AzureTagName tag_value=$AzureTagValue tenant_id=$AzureTenantId client_id=$AzureClientId subscription_id=$AzureSubscriptionId secret_access_key=$AzureSecret`"]}" | Out-File .\config\azure.json -NoNewLine -Encoding default
+}
+
+Start-Process -FilePath "nssm.exe" -ArgumentList "install consul $path\consul.exe agent -advertise=$localnodeIP -client=$localnodeIP -config-dir=config $ConsulArgs"
+Write-host "install consul $path\consul.exe agent -advertise=$localnodeIP -client=$localnodeIP -config-dir=config $ConsulArgs"
